528bca8f7b2a2f1ba296668530bbf4835c38a4d4
Luckyaremu <aremu.baba70@gmail.com>
2021-04-12 11:43:32 +0100

    global style added

--- src/App.js
+++ src/App.js
@@ -2,11 +2,12 @@ import React from "react";
 import './App.css';
 import Navbar from "./components/Navbar";
 import {BrowserRouter as Router} from "react-router-dom";
+import { GlobalStyle } from "./globalStyle";
 
 function App() {
   return (
     <Router>
-
+      <GlobalStyle />
     <Navbar />
 
     </Router>
