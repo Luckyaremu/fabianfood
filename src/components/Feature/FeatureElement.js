1e6065f1556482091539f68f8d80da3c7a091d9a
Luckyaremu <aremu.baba70@gmail.com>
2021-04-14 20:10:28 +0100

    project done

--- src/components/Feature/FeatureElement.js
+++ src/components/Feature/FeatureElement.js
@@ -1,5 +1,5 @@
 import styled from "styled-components";
-import FeaturePic from "../../image/ImgBg.JPG";
+import FeaturePic from "../../image/pexels-anna-tukhfatullina-food-photographerstylist-2750558.jpg";
 
 export const FeatureContainer = styled.div`
 background: linear-gradient(to right, rgba(0,0,0,0.7), rgba(0,0,0.1)), url(${FeaturePic});
--- src/components/Products/data.js
+++ src/components/Products/data.js
@@ -1,9 +1,9 @@
-import product1 from "../../image/ImgBg.JPG";
-import product2 from "../../image/ImgBg.JPG";
-import product3 from "../../image/ImgBg.JPG";
-import product4 from "../../image/ImgBg.JPG";
-import product5 from "../../image/ImgBg.JPG";
-import product6 from "../../image/ImgBg.JPG";
+import product1 from "../../image/pexels-daria-shevtsova-704569.jpg";
+import product2 from "../../image/pexels-dzenina-lukac-1583884.jpg";
+import product3 from "../../image/pexels-nerfee-mirandilla-3186654.jpg";
+import product4 from "../../image/pexels-sebastian-coman-photography-3655916.jpg";
+import product5 from "../../image/pexels-snapwire-286283.jpg";
+import product6 from "../../image/pexels-anna-tukhfatullina-food-photographerstylist-2750558.jpg";
 
 
 export const productData = [
